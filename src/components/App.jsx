effd9409d0675513784e8dbce85ea5a2534eaa90
jachiu555 <jachiu555@gmail.com>
2019-09-17 12:32:06 -0700

    Got VideoList, VideoListEntry, and Video Player to render on page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,21 @@
 import VideoList from './VideoList.js';
-var App = () => (
-  <div>
+import VideoPlayer from './VideoPlayer.js';
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      currentVideo: 1
+    }
+  }
+
+  whenVideoChange(n) {
+    this.setState({
+      currentVideo: n
+    });
+  }
+  render = () => (
+    <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
         <div><h5><em>search</em> view goes here</h5></div>
@@ -8,14 +23,33 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><VideoPlayer video={props.data}/></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><VideoList /></h5></div>
+        <div><h5><VideoList videos={props.data}/></h5></div>
       </div>
     </div>
   </div>
-);
+  )
+}
+
+// var App = (props) => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><VideoPlayer video={props.data}/></h5></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><h5><VideoList videos={props.data}/></h5></div>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
