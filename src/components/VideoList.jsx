03f9a66cd27425cc02c3c11ddf56587d5aae42d6
jachiu555 <jachiu555@gmail.com>
2019-09-17 12:33:02 -0700

    Mapped the exampleVideoData object onto the entries.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,12 @@
-//import exampleVideoData from "../data/exampleVideoData.js";
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js'
+var VideoList = (props) => (
   <div className="video-list">
+    {props.videos.map((obj)=><VideoListEntry video={obj}/>)}
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
