c4a21fcd33071f26224c1a3a59b905ccf194e376
jachiu555 <jachiu555@gmail.com>
2019-09-17 17:26:52 -0700

    Included the videoOnClick method.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,11 +2,6 @@ import VideoListEntry from './VideoListEntry.js'
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map((obj)=><VideoListEntry video={obj} videoOnClick={props.videoOnClick}/>)}
-    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
